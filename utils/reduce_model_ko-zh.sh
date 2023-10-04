1b2a47ef08814f94e5963ed9b20924660f72ece9
jack <ysj@rmsoft.kr>
2023-10-04 02:15:48 +0000

    2023.10.04 일일 업무

--- utils/reduce_model_ko-zh.sh
+++ utils/reduce_model_ko-zh.sh
@@ -35,3 +35,8 @@ python $PRUNE_MODEL \
 --ft-dict /home/scripts/ft/dict.txt \
 --langs ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN \
 --output /home/scripts/reduced_model
+
+python $TSV_TO_JSON \
+--split_path $SPLITS_DIR \
+--source_lang $SOURCE_LANG \
+--target_lang $TARGET_LANG
