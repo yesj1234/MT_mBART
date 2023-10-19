e24b0364096496eb55dd06b4bf7681013ac8e49e
jack <ysj@rmsoft.kr>
2023-10-19 01:17:01 +0000

    1019

--- utils/prepare_data.sh
+++ utils/prepare_data.sh
@@ -5,15 +5,16 @@ export TSV_TO_JSON=/home/ubuntu/MT_mBART/utils/2.tsv_to_json.py
 export REFINE_DATA=/home/ubuntu/MT_mBART/utils/refine_data.py
 export SOURCE_LANG=ko
 export TARGET_LANG=en
-export SPLITS_DIR=/home/ubuntu/중간_한국어_중국어/mt_split
+export SPLITS_DIR=/home/ubuntu/'한국어(KO)_영어(EN)'/mt_split
 
 python3 $PREPARE_FROM_JSON_MT \
---mt_dest_file /home/ubuntu/중간_한국어_중국어 \
---jsons /home/ubuntu/중간_한국어_중국어/
+--mt_dest_file /home/ubuntu/'한국어(KO)_영어(EN)' \
+--jsons /home/ubuntu/'한국어(KO)_영어(EN)'/ \
+--ratio 0.05
 
 python3 $REFINE_DATA \
---tsv_splits_dir /home/ubuntu/중간_한국어_중국어/mt_split \
---langs ko-zh
+--tsv_splits_dir /home/ubuntu/'한국어(KO)_영어(EN)'/mt_split \
+--langs ko-en
 
 python3 $TSV_TO_JSON \
 --split_path $SPLITS_DIR \
