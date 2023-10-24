24cfe503719d65b99b7037092697e99a02a75e17
jack <juesc007@gmail.com>
2023-10-24 18:03:48 +0900

    1024

--- utils/prepare_data.sh
+++ utils/prepare_data.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash 
 
-export PREPARE_FROM_JSON_MT=/home/ubuntu/MT_mBART/utils/1.prepare_from_json_mt.py
-export TSV_TO_JSON=/home/ubuntu/MT_mBART/utils/2.tsv_to_json.py
+export PREPARE_FROM_JSON_MT=/home/ubuntu/MT_mBART/utils/prepare_from_json_mt.py
+export TSV_TO_JSON=/home/ubuntu/MT_mBART/utils/tsv_to_json.py
 export REFINE_DATA=/home/ubuntu/MT_mBART/utils/refine_data.py
 export SOURCE_LANG=ko
 export TARGET_LANG=en
