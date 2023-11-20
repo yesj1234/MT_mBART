cf15bd4a4ba2155a6d55ced2cbca0cfb2b8ae43c
jack <ysj@rmsoft.kr>
2023-11-20 07:17:31 +0000

    1120

--- utils/tsv_to_json.py
+++ utils/tsv_to_json.py
@@ -3,8 +3,12 @@ import os
 import csv
 import json
 import traceback
+import sys
+
 
 def main(args):
+    csv.field_size_limit(sys.maxsize)
+
     # 1. load split.tsv
     for root, _, files in os.walk(args.split_path):
         for file in files:
@@ -13,21 +17,23 @@ def main(args):
                 # 2. generate json from the split.tsv
                 new_json = {}
                 rows = []
+                count = 0
                 with open(os.path.join(root, file), "r+", encoding="utf-8") as f:
                     split = csv.reader(f, delimiter="\n")
-                    try:
-                        for row in split:
-                            if row:
+                    for row in split:
+                        if row:
+                            try:
                                 source_lang, target_lang = row[0].split(" :: ")
                                 rows.append({f"{args.source_lang}": source_lang, f"{args.target_lang}": target_lang})
-                    except Exception as e:
-                        print(traceback.print_exc())
-                        pass 
+                                count += 1
+                            except Exception as e:
+                                print(traceback.print_exc())
+                                pass
                 new_json["translation"] = rows
                 #3. dump split.json
                 with open(os.path.join(root, f"{split_name}.json"), "w+", encoding="utf-8") as js:
                     json.dump(new_json, js, ensure_ascii=False)
-
+                print(f"generated {split_name}.json with {count} examples")
 
 if __name__ == "__main__":
     import argparse
